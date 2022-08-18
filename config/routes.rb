9171e25bf2d79a05895d0472b2a4a77742eb07ef
gbarazzutti <gbarazzutti2@gmail.com>
2022-08-18 10:13:51 -0300

    template installed. Product scaffold added. Migration ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "dashboard#index"
+  root 'dashboard#index'
+  resources :products
 end
